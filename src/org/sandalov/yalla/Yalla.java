6d997eecfa76617477bc2455028ca62184ff4d8a
Dmitry Sandalov <dmitry@sandalov.org>
2012-03-13 17:17:39 +0400

    open file

--- src/org/sandalov/yalla/Yalla.java
+++ src/org/sandalov/yalla/Yalla.java
@@ -1,14 +1,21 @@
 package org.sandalov.yalla;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import org.sandalov.yalla.R;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.util.Log;
 
 public class Yalla extends Activity {
     @Override
@@ -51,7 +58,24 @@ public class Yalla extends Activity {
 				}
 			)
 			.show();
-	}	
+	}
+	
+	public void openFile(View view) {
+		try{
+			   File f = new File(Environment.getExternalStorageDirectory()+"/filename.txt");			 
+			   FileInputStream fileIS = new FileInputStream(f);			 
+			   BufferedReader buf = new BufferedReader(new InputStreamReader(fileIS));			 
+			   String readString = new String();			 
+			   //just reading each line and pass it on the debugger			 
+			   while((readString = buf.readLine())!= null){			 
+			      Log.d("line: ", readString);			 
+			   }			 
+			} catch (FileNotFoundException e) {			 
+			   e.printStackTrace();			 
+			} catch (IOException e){			 
+			   e.printStackTrace();			 
+			}
+	}
 
 	/** Called when the activity is first created. */
     @Override

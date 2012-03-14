aec418e40416e3607447b9755e5e74510bd44f35
Dmitry Sandalov <dmitry@sandalov.org>
2012-03-14 12:12:59 +0400

    dic load

--- src/org/sandalov/yalla/Yalla.java
+++ src/org/sandalov/yalla/Yalla.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import org.sandalov.yalla.R;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -59,21 +60,39 @@ public class Yalla extends Activity {
 			)
 			.show();
 	}
+
+	public void placeholderDialogCustomText(String text) {
+		new AlertDialog.Builder(this)
+			.setTitle(R.string.placeholder_title)
+			.setMessage(text)
+			.setPositiveButton(R.string.str_ok,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialoginterface, int i)	{}
+				}
+			)
+			.show();
+	}
 	
-	public void openFile(View view) {
+	public void openFile(View view) {		
 		try{
-			   File f = new File(Environment.getExternalStorageDirectory()+"/filename.txt");			 
-			   FileInputStream fileIS = new FileInputStream(f);			 
-			   BufferedReader buf = new BufferedReader(new InputStreamReader(fileIS));			 
-			   String readString = new String();			 
-			   //just reading each line and pass it on the debugger			 
-			   while((readString = buf.readLine())!= null){			 
-			      Log.d("line: ", readString);			 
-			   }			 
-			} catch (FileNotFoundException e) {			 
-			   e.printStackTrace();			 
-			} catch (IOException e){			 
-			   e.printStackTrace();			 
+			   File f = new File(Environment.getExternalStorageDirectory()+"/yalla/Dictionary.dic");
+			   FileInputStream fileIS = new FileInputStream(f);
+			   BufferedReader buf = new BufferedReader(new InputStreamReader(fileIS));
+			   String readString = new String();
+			   //just reading each line and pass it on the debugger
+			   ArrayList<String> wordsInFile = new ArrayList<String>();
+			   while((readString = buf.readLine())!= null){				   
+				   wordsInFile.add(readString);
+			   }
+//			   for (int i=0; i<wordsInFile.size(); i++) {
+//				   Log.d("yalla", wordsInFile.get(i));
+//			   }
+			   int randomNum = (int) Math.ceil(Math.random() * wordsInFile.size()-1);
+			   placeholderDialogCustomText(wordsInFile.get(randomNum));
+			} catch (FileNotFoundException e) {
+			   e.printStackTrace();
+			} catch (IOException e){
+			   e.printStackTrace();
 			}
 	}
 
